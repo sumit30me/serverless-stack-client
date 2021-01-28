afeea09031f3c694684e8132a55c7cc0ffd72bec
Sumit Meshram <sumit@Rakeshs-MacBook-Pro-2.local>
2021-01-28 14:29:49 +0530

    Adding code for note client.

---
+++ src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NotFound.css';
+
+export default function NotFound () {
+    return (
+        <div className="NotFound text-center">
+            <h1>Sorry, page not found!</h1>
+        </div>
+    );
+}
