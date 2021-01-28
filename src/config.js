b111ef025658ce3967036368d11f5d594b065984
Sumit Meshram <sumit@Rakeshs-MacBook-Pro-2.local>
2021-01-28 14:51:13 +0530

    Adding aws amplify configuration.

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "ap-south-1",
+      BUCKET: "sam-test-proj",
+    },
+    apiGateway: {
+      REGION: "ap-south-1",
+      URL: "https://ixlodumt5j.execute-api.ap-south-1.amazonaws.com/dev",
+    },
+    cognito: {
+      REGION: "ap-south-1",
+      USER_POOL_ID: "ap-south-1_oUFwyg1Xh",
+      APP_CLIENT_ID: "1peu6n59vebirfibp28otdge46",
+      IDENTITY_POOL_ID: "ap-south-1:81891b38-5676-44f0-b789-e71514c72b85",
+    },
+  };
+  
+  export default config;
