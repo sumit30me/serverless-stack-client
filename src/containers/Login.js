f9c9d6cbe44b582031e1eaa9f3219d3ff34d2bee
Sumit Meshram <sumit@Rakeshs-MacBook-Pro-2.local>
2021-02-02 14:19:49 +0530

    Adding final changes.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,13 +3,11 @@ import { Auth } from 'aws-amplify';
 import Form from 'react-bootstrap/Form';
 import LoaderButton from '../components/LoaderButton';
 import { useAppContext } from '../libs/contextLib';
-import { useHistory } from 'react-router-dom';
 import { useFormFields } from '../libs/hooksLib';
 import { onError } from '../libs/errorLib';
 import './Login.css';
 
 export default function Login () {
-    const history = useHistory();
     const { userHasAuthenticated } = useAppContext();
     const [isLoading, setIsLoading] = useState(false);
     const [fields, handleFieldChange] =     useFormFields({
@@ -29,7 +27,6 @@ export default function Login () {
         try {
             await Auth.signIn(fields.email, fields.password);
             userHasAuthenticated(true);
-            history.push('/');
         } catch (e) {
             onError(e);
             setIsLoading(false);
