f1a1faf2485f5acb1b7d72fa5d84439b374aaee7
Sumit Meshram <sumit@Rakeshs-MacBook-Pro-2.local>
2021-02-02 13:01:49 +0530

    Adding pages to add/edit/delete/list notes

--- src/config.js
+++ src/config.js
@@ -13,6 +13,8 @@ const config = {
       APP_CLIENT_ID: "1peu6n59vebirfibp28otdge46",
       IDENTITY_POOL_ID: "ap-south-1:81891b38-5676-44f0-b789-e71514c72b85",
     },
+    MAX_ATTACHMENT_SIZE: 5000000,
+    STRIPE_KEY: "pk_test_51IEAdoLw3QKkIrdMtoxbaWAfF87bY4F5ix34Lg9A7JRFI9JFQOZ0Ms0SAJCwoVrjQXOhzODLbuHZiPYuJYeZMRmf005w2yeEE7"
   };
   
   export default config;
